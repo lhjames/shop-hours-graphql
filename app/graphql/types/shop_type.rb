93aafc076d633fa7c58b39e8a73307e2624ca84c
Lucille James <lucille.james00@gmail.com>
2021-03-15 11:10:11 +0100

    modified GraphQl Shop object, created first query, updated readme

--- app/graphql/types/shop_type.rb
+++ app/graphql/types/shop_type.rb
@@ -2,6 +2,6 @@ module Types
   class ShopType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
-    field :hours, Types::TextType, null: false
+    # field :hours, Types::TextType, null: false
   end
 end
